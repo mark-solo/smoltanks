89c0ad0095cf6d27dc18081c7567c22a3641f19f
mark-solo <mark.an.impostor@gmail.com>
2019-08-09 17:12:35 +0300

    tank turret

--- objects/player.lua
+++ objects/player.lua
@@ -4,11 +4,11 @@ function Player:new(x, y, w, h)
   self.x = x or love.graphics.getWidth()/2
   self.y = y or love.graphics.getHeight()/2
   self.w = w or 50
-  self.h = h or 100
+  self.h = h or 90
   self.angle = 0
 
   self.da = 0
-  self.turnSpeed = 10
+  self.turnSpeed = 5
   self.ds = 0
   self.moveSpeed = 100
 end
@@ -35,13 +35,28 @@ end
 
 function Player:draw()
   love.graphics.push()
-
   love.graphics.translate(self.x, self.y)
   love.graphics.rotate(self.angle-math.pi/2)
 
   love.graphics.setColor(1, 1, 1)
   love.graphics.line(0, 0, 0, self.h/2)
   love.graphics.rectangle('line', -self.w/2, -self.h/2, self.w, self.h)
+
+  love.graphics.pop()
+
+  --
+
+  love.graphics.push()
+  love.graphics.translate(self.x, self.y)
+
+  local mouseX, mouseY = love.mouse.getPosition()
+  local gun_angle = math.atan2(mouseY-self.y, mouseX-self.x)
+  love.graphics.line(0, 0, 75*math.cos(gun_angle), 75*math.sin(gun_angle))
+  love.graphics.line(0, 0, 25*math.cos(gun_angle-math.pi/2.5), 25*math.sin(gun_angle-math.pi/2.5))
+  love.graphics.line(0, 0, 25*math.cos(gun_angle+math.pi/2.5), 25*math.sin(gun_angle+math.pi/2.5))
+
+  love.graphics.rotate(gun_angle)
+  love.graphics.circle('line', 0, 0, 25, 5)
 
   love.graphics.pop()
 end
