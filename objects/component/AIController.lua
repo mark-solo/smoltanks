95176d72efb8e3f6c26534b718c6ddea49cd2f4b
mark-solo <clueless.magician@gmail.com>
2019-09-08 21:38:03 +0300

    now aitank move through path

--- objects/component/AIController.lua
+++ objects/component/AIController.lua
@@ -1,20 +1,34 @@
 AIController = Object:extend()
 
 function AIController:new()
-  self.targetX = TILE_SIZE*10
-  self.targetY = TILE_SIZE*5
+  self.targetX = 10
+  self.targetY = 5
 end
 
 function AIController:input(tank)
-  tank:setTurretTo(dirToAngle(self.targetX-tank.x, self.targetY-tank.y))
-  local dirAngle = dirToAngle(self.targetX-tank.x, self.targetY-tank.y)-tank.angle
-  tank:turn(dirAngle)
+  if not tank.path then
+    local start = tank.level.map:pointToIndex(tank.level.map:worldToPoint(tank.x, tank.y))
+    local goal = tank.level.map:pointToIndex(self.targetX, self.targetY)
+    tank.path = tank.level:aStar(start, goal)
+    tank.pathIndex = 1
+  else
+    local currentPoint = tank.path[tank.pathIndex]
+    local cx, cy = tank.level.map:indexToPoint(currentPoint)
+    local cx = (cx+0.5)*TILE_SIZE
+    local cy = (cy+0.5)*TILE_SIZE
 
-  if not self:isCloseToTarget(tank, TILE_SIZE/2) then
-    tank:move(1)
+    tank:setTurretTo(dirToAngle(cx-tank.x, cy-tank.y))
+    local dirAngle = dirToAngle(cx-tank.x, cy-tank.y)-tank.angle
+    tank:turn(dirAngle)
+
+    if self:isCloseToTarget(tank, cx, cy, TILE_SIZE) then
+      if tank.pathIndex+1 <= #tank.path then
+        tank.pathIndex = tank.pathIndex + 1
+      end
+    else
+      tank:move(1)
+    end
   end
-
-  tank.path = tank.level:aStar(tank.level.map:pointToIndex(tank.level.map:worldToPoint(tank.x, tank.y)), tank.level.map:pointToIndex(tank.level.map:worldToPoint(self.targetX, self.targetY)))
 end
 
 function AIController:draw(tank)
@@ -29,8 +43,8 @@ end
 
 -- sensors
 
-function AIController:isCloseToTarget(tank, distance)
+function AIController:isCloseToTarget(tank, tx, ty, distance)
   local distance = distance or TILE_SIZE
-  local real_distance = math.sqrt(math.pow(tank.x-self.targetX, 2), math.pow(tank.y-self.targetY, 2))
+  local real_distance = math.sqrt(math.pow(tank.x-tx, 2), math.pow(tank.y-ty, 2))
   return real_distance < distance
 end
