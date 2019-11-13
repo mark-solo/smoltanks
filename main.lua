daa32b291cc61b757dafd85e870a59f67b715211
mark-solo <mark.an.impostor@gmail.com>
2019-11-13 21:17:39 +0300

    reset

--- main.lua
+++ main.lua
@@ -1,87 +1,29 @@
--- libs
 Object = require "lib.classic"
 Input = require "lib.input"
 wf = require "lib.windfield"
 inspect = require "lib.inspect"
 json = require "lib.json"
 
--- constants
 DEBUG = true
 TILE_SIZE = 64
 
--- core game local variable
-local fixedUpdateRate = 0.02
-local fixedUpdateTimer = 0
-
-local log_text = {}
-local last_logline_repeated = 0
-
-mousePressed = false
-wasPressed = false
-
 function love.load()
-	--love.window.setMode(640, 480)
 	love.window.setMode(TILE_SIZE*8, TILE_SIZE*8)
-	log('init')
 
 	local object_files = {}
   recursiveEnumerate('objects', object_files)
   requireFiles(object_files)
-
-	world = wf.newWorld(0, 0, true)
-	if (DEBUG) then world:setQueryDebugDrawing(true) end
-  world:addCollisionClass('Tank')
-  world:addCollisionClass('Bullet', {ignores = {'Bullet'}})
-  world:addCollisionClass('Wall')
-  world:addCollisionClass('Spawn', {ignores = {'Spawn'}})
-	world:addCollisionClass('Flag')
-	world:addCollisionClass('Sensor', {'All', except={'Spawn'}})
-
-  --world = wf.newWorld(0, 0, true)
-	r = ResourceManager()
-
-	menuScene = MenuScene()
-	gameScene = GameScene()
-	Scene.setScene(menuScene)
-
-	camera = Camera()
 end
 
 function love.update(dt)
-	mousePressed = love.mouse.isDown(1)
-
-	scene:input()
-	scene:update(dt)
-	world:update(dt)
-
-	fixedUpdateTimer = fixedUpdateTimer + dt
-	if fixedUpdateTimer > fixedUpdateRate then
-		scene:fixedUpdate(fixedUpdateTimer)
-		--world:update(fixedUpdateTimer)
-		fixedUpdateTimer = 0
-	end
-
-	wasPressed = mousePressed
 end
 
 function love.draw()
-	--camera:apply()
-	scene:render()
-	--love.graphics.draw(sprites['red'], TILE_SIZE*2, TILE_SIZE*2)
-
-	if DEBUG then
-		world:draw(0.2)
-
-		love.graphics.setColor(1, 1, 1)
-		love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), cameraToWorld(love.graphics.getWidth()-50, 0))
-	end
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.rectangle('fill', TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE)
 end
 
-function love.event.quit()
-	world.destroy()
-end
-
----------
+----------
 
 function recursiveEnumerate(folder, file_list)
     local items = love.filesystem.getDirectoryItems(folder)
@@ -101,41 +43,4 @@ function requireFiles(files)
         local file = file:sub(1, -5)
         require(file)
     end
-end
-
---
-
-function log(text, type)
-	local type = type or 'info'
-
-	local last_line = log_text[1]
-	if last_line~=nil and last_line.text == text and last_line.type == type then
-		last_logline_repeated = last_logline_repeated + 1
-	else
-		local message = {}
-
-		if last_logline_repeated > 1 then
-			message.text = 'last message was repeated '..last_logline_repeated..' times'
-			message.type = 'warning'
-		else
-			message.text = text
-			message.type = type
-		end
-
-		table.insert(log_text, 1, message)
-		if #log_text > 50 then table.remove(log_text) end
-		last_logline_repeated = 0
-	end
-end
-
-function draw_log(x, y)
-	local colors_for_types = {}
-	colors_for_types['info'] = {1, 1, 1, 0.5}
-	colors_for_types['warning'] = {1, 1, 0, 0.75}
-	colors_for_types['error'] = {1, 0, 0, 1}
-
-	for i, message in ipairs(log_text) do
-		love.graphics.setColor(colors_for_types[message.type])
-		love.graphics.print(message.text, x, y+(i-1)*12)
-	end
 end
